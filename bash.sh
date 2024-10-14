755a4206c623b5bfaf3c9bac54ea292a6a62e5cc
openwrt2223 <openwrt2223@danwin1210.de>
2024-10-14 15:54:59 +0800

    上传

--- bash.sh
+++ bash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-wget https://dl.google.com/go/go1.23.2.linux-amd64.tar.gz -O $HOME/go.tar.gz
-tar -zxf $HOME/go.tar.gz
-rm -rf $HOME/go.tar.gz
-export GOROOT=$HOME/go
-mkdir -p $HOME/golang
-export GOPATH=$HOME/golang
-export PATH=$GOROOT/bin:$PATH
+wget https://dl.google.com/go/go1.23.2.linux-amd64.tar.gz -O $GITHUB_WORKSPACE/go.tar.gz
+tar -zxf $GITHUB_WORKSPACE/go.tar.gz
+rm -rf $GITHUB_WORKSPACE/go.tar.gz
+export GOROOT=$GITHUB_WORKSPACE/go
+mkdir -p $GITHUB_WORKSPACE/golang
+export GOPATH=$GITHUB_WORKSPACE/golang
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 go install golang.org/x/mobile/cmd/gomobile@latest
 gomobile init
 make all
